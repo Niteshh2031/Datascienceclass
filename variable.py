ba877598aa37ca4b824ee7173f0ff730ecfaae6b
unknown <ntimsina001@gmail.com>
2025-06-27 15:50:27 +0545

    snippets of code on variable

---
+++ variable.py
@@ -0,0 +1,6 @@
+#assigning message to a variable and printing it
+message="This is python code!"
+print(message)
+#changing variable message and printing it 
+message="This is the updated python code!"
+print(message)
