4c4d9e3e3061c168571d3bc1e4cb04a6b2da1bf6
unknown <ntimsina001@gmail.com>
2025-06-27 15:52:09 +0545

    Snippets of code on numeric data types:integer and float and operation

---
+++ number.py
@@ -0,0 +1,6 @@
+#this is on numbers. The following code all try to obtain number 8 as a result
+print(5+3)
+print(4*2)
+print(16/2)#division always results in float
+print(2**3)
+print(12.0-4)#using float in operation results in floating output
